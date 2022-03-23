8ed6768f1b83dc09b1afca516612051c60e6cadf
Magnus Brantheim <m.brantheim@gmail.com>
2022-03-23 20:35:23 +0100

    Deployed ftm-wbtc

--- scripts/deploy/1-vault.js
+++ scripts/deploy/1-vault.js
@@ -1,12 +1,12 @@
 async function main() {
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
 
-  const wantAddress = '0x2599Eba5fD1e49F294C76D034557948034d6C96E';
-  const tokenName = 'FTM-DEUS Spirit LiquidV2 Crypt';
-  const tokenSymbol = 'rf-FTM-DEUS-l-s';
+  const wantAddress = '0x279b2c897737a50405ED2091694F225D83F2D3bA';
+  const tokenName = 'FTM-WBTC Spirit LiquidV2 Crypt';
+  const tokenSymbol = 'rf-FTM-WBTC-l-s';
   const depositFee = 0;
   const tvlCap = ethers.constants.MaxUint256;
-  const options = {gasPrice: 200000000000, gasLimit: 9000000};
+  const options = {gasPrice: 400000000000, gasLimit: 9000000};
 
   const vault = await Vault.deploy(wantAddress, tokenName, tokenSymbol, depositFee, tvlCap, options);
 
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,10 +1,10 @@
 async function main() {
-  const vaultAddress = '0xe040634Ce403341eB5BfF36B88A1Cd2dF665773a';
-  const strategyAddress = '0x6E3e3e8824A4CB6FAbcBBe50705E595FD8FfD1F7';
+  const vaultAddress = '0xA34276e30c4b793c4f6A5Bf77D3973B510CE63e3';
+  const strategyAddress = '0x39f2E23fBc5Cd4D4597cCeca5614745ACB355bf9';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
-  const options = {gasPrice: 200000000000, gasLimit: 9000000};
+  const options = {gasPrice: 400000000000, gasLimit: 9000000};
 
   await vault.initialize(strategyAddress, options);
   console.log('Vault initialized');
